1eb08eaea6f24f35d93d8a4d93214fb9d9a57db9
Rodrigo Sanguanini <rodrigokiller@gmail.com>
2019-10-10 15:42:33 +0200

    new

--- src/index.js
+++ src/index.js
@@ -12,16 +12,23 @@ class Square extends React.Component {
     
     render() {
         return (
-            <button className="square" onClick={() => alert('click')}>
-                {this.props.value}
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
+                {this.state.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
     renderSquare(i) {
-        return <Square value={i}/>;
+        return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
     }
 
     render() {
